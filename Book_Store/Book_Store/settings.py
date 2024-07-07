0d7afcbc3a0cab5ca4d6a0e19c1383298f0ceb99
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-07-07 19:25:36 +0300

    crispy

--- Book_Store/Book_Store/settings.py
+++ Book_Store/Book_Store/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'crispy_forms'
+    'crispy_bootstrap5'
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,5 @@ STATICFILES_DIR=[
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'

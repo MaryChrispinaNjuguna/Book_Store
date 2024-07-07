ebcd9aab7e869fe37e36bef8509512cac751e863
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-07-07 18:18:52 +0300

    super user

--- Book_Store/Book_Store/settings.py
+++ Book_Store/Book_Store/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL ='/images'
+MEDIA_URL ='/images/'
 
 STATICFILES_DIR=[
     os.path.join(BASE_DIR, "static")

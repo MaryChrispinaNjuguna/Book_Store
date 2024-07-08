8b5f9df141f0c58b70f7c89f3367e24281969409
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-07-08 21:51:23 +0300

    forms

--- Book_Store/Book_Store/settings.py
+++ Book_Store/Book_Store/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'crispy_forms'
+    'crispy_forms',
     'crispy_bootstrap5'
 
 ]
--- Book_Store/users/views.py
+++ Book_Store/users/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+
+
 
 def home(request):
     return render(request, 'home.html')
     
+def register(request):
+    if request.method=="POST":
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            return redirect('home')
+    else:
+        form=UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
+

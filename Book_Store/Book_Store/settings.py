613dc5ed5d3183f3f8cfc81cff1535fbf23feea5
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-07-11 22:26:54 +0300

    mysql

--- Book_Store/Book_Store/settings.py
+++ Book_Store/Book_Store/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Book_Store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Book_Store',
+        'USER': 'root',
+        'PASSWORD':'Njuguna2',
+        'HOST': '127.0.0.1',
+        'PORT':'3306'
     }
 }
 

b16138eeeea225133975a23635b8dcffbdb67c33
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-07-12 22:33:11 +0300

    mysql

--- Book_Store/users/models.py
+++ Book_Store/users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
+class Users(models.Model):
     email=models.EmailField(max_length=30)
-    password=models.PasswordField(max_length=30)
-    
+    password=models.CharField(max_length=50)
